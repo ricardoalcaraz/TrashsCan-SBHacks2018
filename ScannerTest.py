9514fd2b6c699c6899e7c3911097c4f3ce6cd4a8
ricardoalcaraz <ralcarazerhs@gmail.com>
2018-01-20 02:16:53 -0800

    Fixed read bug where it wuldn't parse correctly

--- ScannerTest.py
+++ ScannerTest.py
@@ -1,5 +1,5 @@
 import csv
-with open('scanner.csv', 'rb') as csvfile:
-    barcodeReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+with open('test.csv', 'r') as csvfile:
+    barcodeReader = csv.reader(csvfile, delimiter='"', quotechar='|')
     for row in barcodeReader:
         print(', '.join(row))
