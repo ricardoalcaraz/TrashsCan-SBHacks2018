f80d3e1ad728bd5d29cdd64f4c09880c7d427a46
ricardoalcaraz <ralcarazerhs@gmail.com>
2018-01-20 18:26:07 -0800

    Shortened sleep time

--- Display/Display.cpp
+++ Display/Display.cpp
@@ -13,7 +13,7 @@
 #define RGB_RED 0xff,0x00,0x00
 #define RGB_GRN 0x00,0xff,0x00
 #define RGB_BLU 0x00,0x00,0xff
-#define SLEEP_TIME 2
+#define SLEEP_TIME 1
 
 using namespace std;
 upm::Jhd1313m1* lcd;
@@ -27,19 +27,7 @@ void display(string str1, string str2, int red, int green, int blue)
 	lcd->write(str1);
 	lcd->setCursor(1,2); /* second row */
 	lcd->write(str2);
-	lcd->sleep(SLEEP_TIME);
-}
-
-/**Will read the first two lines of a text file and output it onto the LCD screen*/
-int main(int argc, char* argv[]) {
-	/*Initialize LCD screen address*/
-	lcd = new upm::Jhd1313m1(I2C_BUS, 0x3e, 0x62);
-	printToScreen();
-	/*Clear memory and empty out LCD screen*/
-        /* Commented out to keep screen on while fetching new data */
-	/* delete lcd; */ 
-
-	return 0;
+	sleep(SLEEP_TIME);
 }
 
 void printToScreen() {
@@ -58,3 +46,17 @@ void printToScreen() {
 		}
 	}
 }
+
+/**Will read the first two lines of a text file and output it onto the LCD screen*/
+int main(int argc, char* argv[]) {
+	/*Initialize LCD screen address*/
+	lcd = new upm::Jhd1313m1(I2C_BUS, 0x3e, 0x62);
+	printToScreen();
+	/*Clear memory and empty out LCD screen*/
+        /* Commented out to keep screen on while fetching new data */
+	/* delete lcd; */ 
+
+	return 0;
+}
+
+
