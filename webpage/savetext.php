0470086b4fe55578bcb2cea51317f2821ffc946d
ricardoalcaraz <ralcarazerhs@gmail.com>
2018-01-21 04:04:34 -0800

    Commited webpage to grab barcode data

---
+++ webpage/savetext.php
@@ -0,0 +1,12 @@
+<?php
+		$data = $_POST['barcode'];
+		$ret = file_put_contents('barcodes.txt', $data."\n", FILE_APPEND | LOCK_EX);
+		if($ret === false) {
+			echo "$data";
+			die('There was an error writing');
+		}
+		else {
+			header("Location: {$_SERVER['HTTP_REFERER']}");
+			exit;
+		}
+?>
